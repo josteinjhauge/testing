3b314eb39ee55f2509ec025512a08c9c49250ddf
Elin Holde Hegsvold <elinholde@gmail.com>
2020-02-19 14:19:03 +0100

    initDB ting

--- TestingBank/InitDB.php
+++ TestingBank/InitDB.php
@@ -1,8 +1,38 @@
 <?php
+header('Content-Type: application/json');
+$OK = true;
+$db = new mysqli("localhost", "root","","Bank");
+if($db->connect_error)
+{
+   $OK=false;
+}
+$fil= file_get_contents('Bank.sql');
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$res = $db->multi_query($fil);
+if($res)
+{
+    do {
+        if ($result = $db->store_result()) {
+            if($result == false)
+            {
+                $OK = false;
+            }
+            $result->free();
+        }
+        $db->more_results();
+    } while ($db->next_result());
 
+}
+else
+{
+    $OK=false;
+}
+$db->close();
+if($OK)
+{
+    echo json_encode("OK");
+}
+else
+{
+    echo json_encode("Feil");
+} 
